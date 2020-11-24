bf9384e72f4bdf6b59dd6bf6ad5e5f369bb16fd5
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2020-11-23 20:36:40 -0400

    Final back-end project part 1

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcos Isaac Cavalcanti','39165702000', 12500.99, TIMESTAMP WITH TIME ZONE '1943-12-25T20:50:07.12345Z', 7);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Samuel Calebe Julio Castro','85191593383', 4721.0, TIMESTAMP WITH TIME ZONE '1986-11-26T10:00:00.54321Z', 1); 
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Andrea Lívia da Conceição', '89568911332', 3188.10, TIMESTAMP WITH TIME ZONE '1984-07-23T10:00:00.32145Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Danilo Filipe Moura', '44967185886', 5500.0, TIMESTAMP WITH TIME ZONE '1947-04-10T10:00:00.42241Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Josefa Bárbara Souza', '93439054675', 8300.47, TIMESTAMP WITH TIME ZONE '1990-10-21T10:00:00.52411Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bianca Sophie Aline Lima', '95738747410', 2350.0, TIMESTAMP WITH TIME ZONE '2000-07-09T10:00:00.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Nair Milena Pereira', '78312740490', 844.77, TIMESTAMP WITH TIME ZONE '2002-03-19T10:00:00.22122Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Jennifer Sophia Luna Melo', '99561255049', 3850.92, TIMESTAMP WITH TIME ZONE '1997-05-21T10:00:00.34231Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Davi Ian Lorenzo','67479035527', 1300.37, TIMESTAMP WITH TIME ZONE '2001-07-29T10:00:00.41212Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Raimundo Fernando Rodrigues', '33290842789', 4500.0, TIMESTAMP WITH TIME ZONE '1977-02-07T10:00:00.11213Z', 2);
